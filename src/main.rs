16c6864be088db485d6397b10c2f7cf3ab3f9cd6
gabriel-sparkes <sparkes.gabo@gmail.com>
2025-08-06 17:00:25 +0200

    WIP: Add cursive view, removed most of the test stuff from main fn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use chrono::{Local, NaiveDateTime, TimeZone};
+use cursive::{views::TextView, Cursive, CursiveExt};
 use notify_rust::Notification;
 use serde::{Deserialize, Serialize};
+use cursive::views::{ScrollView, LinearLayout};
 use std::{
     env::home_dir,
-    fs::{self, read_to_string, write},
+    fs::{self, File, read_to_string, write},
     io::{self, Write},
     path::Path,
     sync::Arc,
@@ -13,8 +15,6 @@ use tokio::{
     sync::Mutex,
     time::{Duration, sleep},
 };
-
-const TASKS_DIR: &str = "/todo/tasks.json";
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 enum Priority {
@@ -28,41 +28,49 @@ struct Task {
     content: String,
     deadline: u64,
     priority: Priority,
+    completed: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tasks_dir: String = match home_dir() {
-        Some(path) => path.display().to_string() + TASKS_DIR,
-        None => TASKS_DIR.to_string(),
+    let mut siv = Cursive::new();
+
+    siv.add_layer(TextView::new("Hello World!\nPress q to quit."));
+    siv.add_global_callback('q', |s| s.quit());
+
+    let working_dir = match home_dir() {
+        Some(path) => path.display().to_string() + "/todo/",
+        None => "/todo/".to_string(),
     };
+    let tasks_location = working_dir.clone() + "tasks.json";
+    let icon_location = working_dir + "icon.png";
+    if !Path::new(&icon_location).exists() {
+        println!("Icon not set");
+    }
 
     // set up arc-mutex to share with ctrlc exit handler
-    let tasks_init = load(&tasks_dir)?;
+    let tasks_init = load(&tasks_location)?;
     println!("Loaded tasks: {:?}", tasks_init);
     let tasks_arc = Arc::new(Mutex::new(tasks_init));
 
-    let mut name = String::new();
-    let mut deadline = String::new();
-
-    // get task name
-    print!("Task name: ");
-    io::stdout().flush().expect("Flush failed!");
-    io::stdin().read_line(&mut name).unwrap();
-
-    // get task deadline
-    print!("Deadline (dd/mm/yyyy HH:MM): ");
-    io::stdout().flush().expect("Flush failed!");
-    io::stdin().read_line(&mut deadline).unwrap();
-
-    let test_task = Task {
+    /* let task = Task {
         content: name.trim().to_string(),
         deadline: timestamp_from_date(deadline.clone()),
         priority: Priority::Medium,
+        completed: false,
     };
 
-    println!("{:?}", test_task);
-    tasks_arc.lock().await.push(test_task);
+    println!("{:?}", task);
+    tasks_arc.lock().await.push(task); */
+
+    let tasks_guard = tasks_arc.lock().await;
+    let mut layout = LinearLayout::vertical();
+    for task in tasks_guard.iter() {
+        layout.add_child(TextView::new(task.content.clone()));
+    }
+    let scrollable = ScrollView::new(layout);
+    siv.add_layer(scrollable);
+    drop(tasks_guard);
 
     // new block so the program doesn't hang
     // limit the scope of the first lock
@@ -72,7 +80,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         for task in tasks_guard.iter() {
             let task_clone = task.clone();
-            handles.push(tokio::spawn(timer(task_clone))); // assuming async fn timer(Task)
+            handles.push(tokio::spawn(timer(task_clone, icon_location.clone())));
         }
 
         for handle in handles {
@@ -80,9 +88,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    siv.run();
+
     signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
     println!("Exiting");
-    let _ = save(&tasks_arc, &tasks_dir).await;
+    let _ = save(&tasks_arc, &tasks_location).await;
 
     Ok(())
 }
@@ -128,25 +138,27 @@ fn timestamp_from_date(deadline: String) -> u64 {
     deadline_timestamp as u64
 }
 
+fn file_exists(path: &str, create: bool) -> Result<bool, io::Error> {
+    let create_path = Path::new(path);
+    if !create_path.exists() {
+        if create {
+            if let Some(parent) = create_path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+            let _ = File::create(path)?;
+            return Ok(true);
+        } else {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
+
 async fn save(arc: &Arc<Mutex<Vec<Task>>>, path: &str) -> io::Result<()> {
     let guard = arc.lock().await;
     let data = &serde_json::to_string(&*guard).unwrap();
-    match write(path, data) {
-        Ok(_) => (),
-        Err(e) => {
-            println!("An error occurred: {}", e);
-            if e.kind() == io::ErrorKind::NotFound {
-                let create_path = Path::new(path);
-
-                // extract parent directory and create it
-                if let Some(parent) = create_path.parent() {
-                    fs::create_dir_all(parent)?;
-                }
-                println!("Created missing directories");
-                let _ = write(path, data);
-            }
-        }
-    }
+    let _ = file_exists(path, true)?;
+    let _ = write(path, data)?;
     println!("Saved tasks");
     Ok(())
 }
@@ -175,7 +187,10 @@ fn load(path: &str) -> io::Result<Vec<Task>> {
 
 // I have no idea what this box thing is yet
 // or + Send + Sync
-async fn timer(task: Task) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn timer(
+    task: Task,
+    icon_location: String,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let date_time = Local.timestamp_opt(task.deadline as i64, 0).unwrap();
     println!(
         "Starting countdown for task {} scheduled for {}",
@@ -184,9 +199,9 @@ async fn timer(task: Task) -> Result<(), Box<dyn std::error::Error + Send + Sync
     // just seeing if it works
     let _ = sleep(Duration::from_secs(5)).await;
     Notification::new()
-        .summary("Task aaa")
-        .body("This will almost look like a real firefox notification.")
-        .icon("firefox")
+        .summary(&task.content)
+        .body("Time's up")
+        .icon(&icon_location)
         .show()?;
     println!("Done");
     Ok(())
